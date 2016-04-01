04e79ef9302c521dba3e70c1e38fd2439d072d25
Martin Tracey <matracey@outlook.ie>
2016-04-01 18:49:22 +0100

    Built Models, Repository and Service classes as in the first section of the course.

---
+++ RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,36 @@
+﻿using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Repository;
+using System.Collections.Generic;
+
+namespace RaysHotDogs.Core.Service
+{
+    public class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            return hotDogRepository.GetFavoriteHotDogs();
+        }
+        
+        public HotDog GetHotDog(int hotDogId)
+        {
+            return hotDogRepository.GetHotDog(hotDogId);
+        }
+    }
+}
